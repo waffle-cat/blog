5f22007ce28bfe68eed07fb99d09282dca3ffb95
Waffle-Cat <xyng735@gmail.com>
2016-09-26 09:47:31 +0800

    First commit.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+	def show
+	end
+
+	
+	def index
+	end
+
+	def new
+	end
+
+	def create 
+		@post = Post.new(post_params)
+
+		if @post.save
+			# redirect_to @post
+			# redirect_to "/posts/#{@post.id}"
+			redirect_to post_path(@post)
+		else
+
+		end
+	end
+
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
+end
